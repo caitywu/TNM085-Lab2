2a6929a839ba1f7a55f58a881f9858f15ab3412a
almli825 <almli825@student.liu.se>
2024-02-06 17:27:41 +0100

    Update particle.h

--- include/particlesystem/particle.h
+++ include/particlesystem/particle.h
@@ -8,13 +8,14 @@
 
 class Particle {
 
-	glm::vec2 position{ 0,0 }; //(x,y) corrdinate 
+	glm::vec2 position{ 0,0 }; //(x,y) coordinate 
 	glm::vec2 velocity{ 0,0 }; // m/s
 	glm::vec4 color{ 255, 255, 255, 1 }; //RGBA
+	float size{ 0.1 }; // radius (m)
 	float life_time{ 10.0 };  //s 
 
 	//constructor
-	Particle(glm::vec2& init_pos, glm::vec2& init_vel, glm::vec4& init_col, float lf) :
-		position(init_pos), velocity(init_vel), color(init_col), life_time(lf) {}
+	Particle(glm::vec2& init_pos, glm::vec2& init_vel, glm::vec4& init_col, float s, float lf) :
+		position(init_pos), velocity(init_vel), color(init_col), size(s), life_time(lf) {}
 
 };
